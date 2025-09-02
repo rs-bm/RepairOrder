bbeb64248939ac7ec9fccfe00d80d938caf5ac99
williamswanson <william.swanson@bruins.belmont.edu>
2025-09-02 12:17:53 -0500

    subheader added

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -1,6 +1,8 @@
 package com.example.repairorder;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class RepairOrderActivity extends AppCompatActivity {
 
+    double numbers = 0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,13 @@ public class RepairOrderActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView totalTV = findViewById(R.id.subtotalField);
+        Random gen = new Random();
+        double number = gen.nextDouble();
+        String w = "$ " + number;
+        totalTV.setText(w);
+        String value = totalTV.getText().toString();
+        Log.i("TEST", value);
     }
 }
